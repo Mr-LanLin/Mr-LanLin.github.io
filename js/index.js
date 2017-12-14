dc878027a8728d070d7c58d38ae8e83e5ec5fd79
lanlin <lanlin0163@163.com>
2017-12-15 01:55:07 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -1,132 +1,130 @@
-var w = c.width = window.innerWidth - 30,
-    h = c.height = document.getElementsByTagName("body")[0].offsetHeight - 870,
-    ctx = c.getContext('2d'),
+try {
+	var w = c.width = window.innerWidth - 30, h = c.height = document.getElementsByTagName("body")[0].offsetHeight - 890, ctx = c.getContext('2d'), opts = {
+		particles : 30,
+		particleBaseSize : 5,
+		particleAddedSize : 5,
+		particleSizeSpeedMultiplier : .2,
+		particleBaseRadiant : Math.PI / 2 - .4,
+		particleAddedRadiant : .2,
 
-    opts = {
-        particles: 50,
-        particleBaseSize: 10,
-        particleAddedSize: 5,
-        particleSizeSpeedMultiplier: .2,
-        particleBaseRadiant: Math.PI / 2 - .4,
-        particleAddedRadiant: .2,
+		trails : 30,
+		trailSizeBaseMultiplier : .6,
+		trailSizeAddedMultiplier : .2,
+		trailSizeSpeedMultiplier : .1,
+		trailAddedBaseRadiant : -1,
+		trailAddedAddedRadiant : 2,
+		trailBaseLifeSpan : 30,
+		trailAddedLifeSpan : 30
 
-        trails: 30,
-        trailSizeBaseMultiplier: .6,
-        trailSizeAddedMultiplier: .2,
-        trailSizeSpeedMultiplier: .1,
-        trailAddedBaseRadiant: -1,
-        trailAddedAddedRadiant: 2,
-        trailBaseLifeSpan: 50,
-        trailAddedLifeSpan: 30
-
-    },
-
-    tau = Math.PI * 2,
-    particles = [];
-
+	}, tau = Math.PI * 2, particles = [];
+} catch(e) {
+}
 function Particle() {
 
-    this.trails = [];
+	this.trails = [];
 
-    this.reset();
+	this.reset();
 }
 
-Particle.prototype.reset = function () {
+Particle.prototype.reset = function() {
 
-    this.size = opts.particleBaseSize + opts.particleAddedSize * Math.random();
+	this.size = opts.particleBaseSize + opts.particleAddedSize * Math.random();
 
-    this.x = Math.random() * w;
-    this.y = -this.size;
+	this.x = Math.random() * w;
+	this.y = -this.size;
 
-    var speed = this.size * opts.particleSizeSpeedMultiplier,
-        rad = opts.particleBaseRadiant + opts.particleAddedRadiant * Math.random();
+	var speed = this.size * opts.particleSizeSpeedMultiplier, rad = opts.particleBaseRadiant + opts.particleAddedRadiant * Math.random();
 
-    this.vx = speed * Math.cos(rad);
-    this.vy = speed * Math.sin(rad);
+	this.vx = speed * Math.cos(rad);
+	this.vy = speed * Math.sin(rad);
 
-    this.rad = rad;
+	this.rad = rad;
 }
-Particle.prototype.step = function () {
+Particle.prototype.step = function() {
 
-    if (this.trails.length < opts.trails && Math.random() < .1)
-        this.trails.push(new Trail(this));
+	if (this.trails.length < opts.trails && Math.random() < .1)
+		this.trails.push(new Trail(this));
 
-    for (var i = 0; i < this.trails.length; ++i)
-        this.trails[i].step();
+	for (var i = 0; i < this.trails.length; ++i)
+		this.trails[i].step();
 
-    this.x += this.vx;
-    this.y += this.vy;
+	this.x += this.vx;
+	this.y += this.vy;
 
-    if (this.x < -this.size)
-        this.x = w + this.size;
-    else if (this.x > w + this.size)
-        this.x = -this.size;
+	if (this.x < -this.size)
+		this.x = w + this.size;
+	else if (this.x > w + this.size)
+		this.x = -this.size;
 
-    if (this.y > h + this.size)
-        this.reset();
+	if (this.y > h + this.size)
+		this.reset();
 
-    ctx.moveTo(this.x, this.y);
-    ctx.arc(this.x, this.y, this.size, 0, tau);
+	ctx.moveTo(this.x, this.y);
+	ctx.arc(this.x, this.y, this.size, 0, tau);
 }
-
 function Trail(parent) {
 
-    this.parent = parent;
-    this.reset();
+	this.parent = parent;
+	this.reset();
 }
 
-Trail.prototype.reset = function () {
+Trail.prototype.reset = function() {
 
-    this.x = this.parent.x;
-    this.y = this.parent.y;
-    this.size = this.parent.size * ( opts.trailSizeBaseMultiplier + opts.trailSizeAddedMultiplier * Math.random() );
+	this.x = this.parent.x;
+	this.y = this.parent.y;
+	this.size = this.parent.size * (opts.trailSizeBaseMultiplier + opts.trailSizeAddedMultiplier * Math.random() );
 
-    var rad = this.parent.rad + opts.trailAddedBaseRadiant + opts.trailAddedAddedRadiant * Math.random(),
-        speed = this.size * opts.trailSizeSpeedMultiplier;
+	var rad = this.parent.rad + opts.trailAddedBaseRadiant + opts.trailAddedAddedRadiant * Math.random(), speed = this.size * opts.trailSizeSpeedMultiplier;
 
-    this.vx = speed * Math.cos(rad);
-    this.vy = speed * Math.sin(rad);
+	this.vx = speed * Math.cos(rad);
+	this.vy = speed * Math.sin(rad);
 
-    this.tick = 0;
-    this.life = ( opts.trailBaseLifeSpan + opts.trailAddedLifeSpan * Math.random() ) | 0;
+	this.tick = 0;
+	this.life = (opts.trailBaseLifeSpan + opts.trailAddedLifeSpan * Math.random() ) | 0;
 }
-Trail.prototype.step = function () {
+Trail.prototype.step = function() {++this.tick;
 
-    ++this.tick;
+	if (this.tick > this.life)
+		return this.reset();
 
-    if (this.tick > this.life)
-        return this.reset();
+	this.x += this.vx;
+	this.y += this.vy;
 
-    this.x += this.vx;
-    this.y += this.vy;
-
-    ctx.moveTo(this.x, this.y)
-    ctx.arc(this.x, this.y, ( 1 - ( this.tick / this.life ) ) * this.size, 0, tau);
+	ctx.moveTo(this.x, this.y)
+	ctx.arc(this.x, this.y, (1 - (this.tick / this.life ) ) * this.size, 0, tau);
 }
-
 function anim() {
 
-    window.requestAnimationFrame(anim);
+	window.requestAnimationFrame(anim);
 
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, w, h);
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, w, h);
 
-    ctx.fillStyle = 'skyblue';
-    ctx.beginPath();
+	ctx.fillStyle = 'skyblue';
+	ctx.beginPath();
 
-    if (particles.length < opts.particles && Math.random() < .1)
-        particles.push(new Particle);
+	if (particles.length < opts.particles && Math.random() < .1)
+		particles.push(new Particle);
 
-    for (var p = 0; p < particles.length; ++p)
-        particles[p].step();
+	for (var p = 0; p < particles.length; ++p)
+		particles[p].step();
 
-    ctx.fill();
+	ctx.fill();
 }
 
 anim();
 
-window.addEventListener('resize', function () {
+window.addEventListener('resize', function() {
+	try {
+		w = c.width = window.innerWidth - 30;
+		h = c.height = document.getElementsByTagName("body")[0].offsetHeight - 890;
+	} catch(e) {
+	}
+});
 
-    w = c.width = window.innerWidth - 30;
-    h = c.height = document.getElementsByTagName("body")[0].offsetHeight - 870;//window.innerHeight;
-})
+if (h <= 0) {
+	c.parentNode.removeChild(c);
+	document.getElementsByClassName("container")[0].style.height = window.innerHeight;
+	// document.getElementsByClassName("container")[0].style.clientHeight = window.innerHeight;
+	// document.getElementsByClassName("container")[0].style.offsetHeight = window.innerHeight;
+};
