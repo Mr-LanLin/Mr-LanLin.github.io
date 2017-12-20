cb81ca81519f7eab4b614bd30d1b117153c9bfa5
兰林 <lanlin@thunisoft.com>
2017-12-20 15:03:49 +0800

    提交

--- js/display.js
+++ js/display.js
@@ -1,4 +1,11 @@
-var url = document.URL;
+var url = document.URL.replace('/display.html?static/source', '');
+url = "https://mr-lanlin.github.io/2017/12/13/Java注解知识整理.html";
 //url = decodeURIComponent(url);
-var displayframe = document.getElementById('display_frame');
-displayframe.src = url.replace('/display.html?static/source', '');
+var minHeight = $(window).height() - 200 + "px";
+$("#container").html('<iframe id="display_frame" frameborder="0" onload="newsize();" src="' + url + '" style="overflow-x: auto !important;min-height: ' + minHeight + '" width="100%" height="100%">\n' +
+    '    </iframe>');
+function newsize() {
+    var displayframe = document.getElementById('display_frame');
+    var docm = displayframe.contentDocument;
+    displayframe.style.height = docm.getElementsByTagName("body")[0].offsetHeight + 100 + "px";
+}
