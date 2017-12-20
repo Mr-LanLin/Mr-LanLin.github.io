114677e75b60d637eaacd743258804b6d4457255
兰林 <lanlin@thunisoft.com>
2017-12-20 18:51:58 +0800

    提交

--- js/display.js
+++ js/display.js
@@ -1,5 +1,6 @@
 var url = document.URL.replace('/display.html?static/source', '');
 //url = decodeURIComponent(url);
+document.getElementById("SOHUCS").sid="url";
 var minHeight = $(window).height() - 200 + "px";
 $("#container").html('<iframe id="display_frame" scrolling="no" sandbox="allow-same-origin" frameborder="0" onload="newsize();" src="' + url + '" style="overflow-x: auto !important;min-height: ' + minHeight + '" width="100%" height="100%">\n' +
     '    </iframe>');
